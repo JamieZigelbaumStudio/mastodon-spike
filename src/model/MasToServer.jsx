f52afbd7764256e95bfb8235ff5d6c0196d3ffd0
Arpan Majumder <arpan.majumdar@thoughtworks.com>
2020-01-07 19:31:34 +0530

    Update mastodon server url

--- src/model/MasToServer.jsx
+++ src/model/MasToServer.jsx
@@ -1,7 +1,7 @@
 export const masToServer = {
-    domain_url: 'https://mas.to',
-    api_url: 'https://mas.to/api/v1',
-    apps_url: 'https://mas.to/api/v1/apps',
+    domain_url: 'https://m.speculativeartsresearch.com',
+    api_url: 'https://m.speculativeartsresearch.com/api/v1',
+    apps_url: 'https://m.speculativeartsresearch.com/api/v1/apps',
     name: 'Spike',
     scopes: 'read write',
     redirectUri: 'http://localhost:3000/'
