a87848b2e1fc86d16295be9bfbb34ead4ab5a0aa
Arpan Majumder <arpan.majumdar@thoughtworks.com>
2020-01-13 22:22:47 +0530

    Add sanitize-html package

--- src/toots/components/Toot.jsx
+++ src/toots/components/Toot.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import parse from "html-react-parser";
+import sanitizeHtml from "sanitize-html"
 
 export const Toot = (props) => {
-    return props.status ? parse(props.status) : "Fetching status"
+    return props.status ? parse(sanitizeHtml(props.status)) : "Fetching status"
 };
