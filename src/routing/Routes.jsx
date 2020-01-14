4f5da0c331ea9a1be1c3859cb4a114dcddd020d7
Arpan Majumder <arpan.majumdar@thoughtworks.com>
2020-01-14 18:36:16 +0530

    Update App component

--- src/routing/Routes.jsx
+++ src/routing/Routes.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import {HashRouter, Route, Switch} from "react-router-dom";
 import {Login} from "../login/components/Login";
 import {Thread} from "../toots/components/Thread";
 
-export const Routes = () => {
-    const loginRoute = <Route exact path="/" component={Login} />;
-    const statusRoute = <Route path="/status/:id" component={Thread} />;
-
-    return <HashRouter>
-        <Switch>
-            {loginRoute}
-            {statusRoute}
-        </Switch>
-    </HashRouter>
-};
+export const Routes = [
+    {
+        path: '/',
+        component: Login,
+        exact: true
+    },
+    {
+        path: '/status/:id',
+        component: Thread,
+        exact: true
+    }];
