3d8286c694095d85548e03671ee9f96870a2ea99
Arpan Majumder <arpan.majumdar@thoughtworks.com>
2020-01-18 01:19:06 +0530

    Add basic assets component

--- src/pages/toots/components/assets/Assets.jsx
+++ src/pages/toots/components/assets/Assets.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import {MenuItem, Select} from '@material-ui/core';
+import {MockAssets} from "../../mock_assets"
 
 export const Assets = () => {
+
+    const [assets] = useState(MockAssets);
+    const [selectedAsset, setSelectedAsset] = useState(assets[0]);
+
+    function handleChange(event) {
+        setSelectedAsset(event.target.value);
+    }
+
     return <div>
         Assets
+        <div>
+            <Select value={selectedAsset} onChange={handleChange}>
+                {assets.map(asset =>
+                    <MenuItem value={asset}>
+                        {asset}
+                    </MenuItem>)}
+            </Select>
+        </div>
     </div>
 };
