8fbf301fa529ffb114c84488e57e29a1a32ca7c6
Arpan Majumder <arpan.majumdar@thoughtworks.com>
2020-01-18 18:24:51 +0530

    Fetch status on page reload

--- src/pages/toots/components/thread/Thread.jsx
+++ src/pages/toots/components/thread/Thread.jsx
@@ -2,13 +2,26 @@ import React, {useState} from "react";
 import {Toot} from "./Toot";
 import {Styled} from "../../styles/thread/Toot"
 import {Assets} from "../assets/Assets";
+import {masToServer} from "../../../common/model/MasToServer";
+import {Mastodon} from "../../../../api/auth/Mastodon";
 
 export const Thread = (props) => {
     const [status, setStatus] = useState();
-    const id = props.location.query.id;
 
     const getStatus = () => {
-        return props.location.mastodon
+        function createMastodonInstance() {
+
+            const tokenInStorage = window.localStorage.getItem('token');
+            return new Mastodon(masToServer.api_url, tokenInStorage);
+        }
+
+        const mastodon = props.location.mastodon ?
+            props.location.mastodon : createMastodonInstance();
+
+        const id = props.location.query ?
+            props.location.query.id : window.location.hash.match(/\d+$/);
+
+        return mastodon
             .fetchStatus(id);
     };
 
