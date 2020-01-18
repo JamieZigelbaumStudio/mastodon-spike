4ac17cb515de4cde095987517607129b12151a3f
Arpan Majumder <arpan.majumdar@thoughtworks.com>
2020-01-18 20:36:58 +0530

    Add share and price view for an asset

--- src/pages/toots/components/assets/asset/components/Asset.jsx
+++ src/pages/toots/components/assets/asset/components/Asset.jsx
@@ -1,8 +1,33 @@
 import React from "react";
-import {AssetWrapper} from "../styles/Asset";
+import {AssetContent, AssetName, AssetPrice, AssetShares, AssetWrapper} from "../styles/Asset";
+import TextField from '@material-ui/core/TextField';
 
 export const Asset = (props) => {
+
+    function displayAssetName() {
+        return <AssetName>
+            Asset Name: {props.asset.name}
+        </AssetName>;
+    }
+
+    function displayAssetShare() {
+        return <AssetShares>
+            Shares available: {props.asset.shares}
+        </AssetShares>;
+    }
+
+    function displayAssetPrice() {
+        return <AssetPrice>
+            Set Asset Price:
+            <TextField required id="standard-required" label="Required" type="number"/>
+        </AssetPrice>;
+    }
+
     return <AssetWrapper>
-        {props.asset}
+        {displayAssetName()}
+        <AssetContent>
+            {displayAssetPrice()}
+            {displayAssetShare()}
+        </AssetContent>
     </AssetWrapper>
 };
--- src/pages/toots/components/assets/asset/styles/Asset.js
+++ src/pages/toots/components/assets/asset/styles/Asset.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
 
 export const AssetWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   flex: 1;
   height: 100%;
-  margin-top: 2.5rem;
+  margin-top: 1.5rem;
+`;
+
+export const AssetName = styled.div`
+  padding-top: 1rem;
+  font-weight: 500;
+`;
+
+export const AssetPrice = styled.div`
+  font-weight: 500;
+  padding-top: 1.5rem;
+`;
+
+export const AssetShares = styled.div`
+  padding-top: 1rem;
+  font-weight: 500;
+`;
+
+export const AssetContent = styled.div`
+  flex: 1;
 `;
